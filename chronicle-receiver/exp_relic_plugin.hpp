51f2d1b7eaac96a95ee6d2d84501c95d5a0e4af4
Ed Rotthoff <ed@fioprotocol.io>
2024-11-22 19:18:21 +0000

    integrate fio.chronicle with relic database for blocks transactions accounts and traces

--- chronicle-receiver/exp_relic_plugin.hpp
+++ chronicle-receiver/exp_relic_plugin.hpp
@@ -1,4 +1,11 @@
 #include <appbase/application.hpp>
+#include "rapidjson/document.h"
+#include "rapidjson/reader.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
+#include <libpq-fe.h>
+#include <iostream>
+#include <fc/log/logger.hpp>
 
 using namespace appbase;
 
@@ -18,4 +25,43 @@ private:
 };
 
 
+const static string UNKNOWN_TIMESTAMP =  "1970-01-01T00:00:00Z";
+  const static string UNKNOWN_STRING = "UNKNOWN";
+  const static string UNKNOWN_NUMBER = "NULL";
+  const static bool ALLOW_EMPTY_VALUES = true;
+  const static bool DISALLOW_EMPTY_VALUES = false;
+
+inline string getjsonstring(string defaultv, rapidjson::Value& v, bool allowempty){
+  string retval = defaultv;
+  ilog("EDEDEDEDEDED IN GETJSON STRING");
+  if (!v.IsNull()){
+    if (v.IsString()){
+      ilog("EDEDEDED DOING A STRING!!!");
+      string tv = v.GetString();
+      if(allowempty)
+      {
+        return tv;
+      }
+      else{
+        if (!tv.empty()){
+          return tv;
+        }
+      }
+
+    }else if (v.IsObject()){
+      ilog("EDEDEDEDEDEDEDED DOING AN OBJECT!!!!");
+       // Create a StringBuffer and a Writer
+        rapidjson::StringBuffer buffer;
+        rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+
+        // Write the Value to the buffer
+        v.Accept(writer);
+
+        // Get the JSON string from the buffer
+        return  buffer.GetString();
+
+    }
+  }
+  return retval;
+}
 
